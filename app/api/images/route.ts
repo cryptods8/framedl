16f053238ba283b3d42551db7e0e88fd559bfb51
ds8 <dusoo.s+ds8@gmail.com>
2024-02-19 21:01:37 +0100

    fix #2

--- app/api/images/route.ts
+++ app/api/images/route.ts
@@ -5,13 +5,18 @@ import { generateImage } from "../../generate-image";
 import { gameService } from "../../game/game-service";
 import { verifySignedUrl } from "../../utils";
 
+const BASE_URL =
+  process.env["VERCEL_ENV"] === "production"
+    ? "https://framedl.vercel.app"
+    : "http://localhost:3000";
+
 function getRequestUrl(req: NextRequest) {
-  const protocol = req.headers.get("x-forwarded-proto") || "https";
-  const host = req.headers.get("x-forwarded-host") || req.headers.get("host");
-  const baseUrl = `${protocol}://${host}`;
+  // const protocol = req.headers.get("x-forwarded-proto") || "https";
+  // const host = req.headers.get("x-forwarded-host") || req.headers.get("host");
+  // const baseUrl = `${protocol}://${host}`;
 
   const url = req.nextUrl;
-  return baseUrl + url.pathname + url.search;
+  return BASE_URL + url.pathname + url.search;
 }
 
 async function renderImageToRes(svg: string): Promise<NextResponse> {
