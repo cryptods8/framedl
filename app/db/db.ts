9b2d8748863eefdcf11ba766667d5856c53835df
ds8 <dusoo.s+ds8@gmail.com>
2024-02-21 15:12:56 +0100

    move redis

--- app/db/db.ts
+++ app/db/db.ts
@@ -1,9 +1,14 @@
-import { kv } from "@vercel/kv";
+import { Redis } from "@upstash/redis";
+
+const redis = new Redis({
+  url: process.env["REDIS_API_URL"]!,
+  token: process.env["REDIS_API_TOKEN"]!,
+});
 
 export const db = {
-  get: async<V> (key: string): Promise<V | null> => kv.get(key),
-  set: async<V> (key: string, value: V): Promise<void> => {
-    await kv.set(key, value);
+  get: async <V>(key: string): Promise<V | null> => redis.get<V>(key),
+  set: async <V>(key: string, value: V): Promise<void> => {
+    await redis.set(key, value);
     return Promise.resolve();
   },
 };
