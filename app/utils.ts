e7fc6388aaa197a418bfddeafae0b5d29826f221
ds8 <dusoo.s+ds8@gmail.com>
2024-03-04 21:33:27 +0100

    version up

--- app/utils.ts
+++ app/utils.ts
@@ -1,18 +1,9 @@
-import signed from "signed";
+import { headers } from "next/headers";
 
-const signingKey = process.env["SIGNING_KEY"];
-if (!signingKey) {
-  throw new Error("SIGNING_KEY is required");
-}
+export function currentURL(pathname: string): URL {
+  const headersList = headers();
+  const host = headersList.get("x-forwarded-host") || headersList.get("host");
+  const protocol = headersList.get("x-forwarded-proto") || "http";
 
-const signature = signed({ secret: signingKey });
-
-export function signUrl(url: string): string {
-  // console.log("SIGNING", url);
-  return signature.sign(url);
-}
-
-export function verifySignedUrl(url: string): string {
-  // console.log("VERIFYING", url);
-  return signature.verify(url);
+  return new URL(pathname, `${protocol}://${host}`);
 }
